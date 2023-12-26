0dd881b7cc20a28a61abffb738f4506426150a4f
hoon <cjsstk805@gmail.com>
2023-12-26 22:23:39 +0900

    - 프로젝트 세팅

---
+++ Source/LON/LON.cpp
@@ -0,0 +1,9 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "LON.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LON, "LON" );
+
+DEFINE_LOG_CATEGORY(LogLON)
+ 
---
+++ Source/LON/LONGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "LONGameMode.generated.h"
+
+UCLASS(minimalapi)
+class ALONGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	ALONGameMode();
+};
+
+
+
